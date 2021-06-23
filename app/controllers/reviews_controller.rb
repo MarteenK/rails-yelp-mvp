c4321b7f98f498a3d74b8a14f0496f10204f8662
martin karlsson <markarlsson@gmail.com>
2021-06-23 19:26:04 -0300

    in progress

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
 end
